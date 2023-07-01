97231767600685487373d58764a0960f68e7ad12
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-07-01 13:30:25 +0100

    first commit

---
+++ src/main/java/com/social/media/confessionmedia/ConfessionMediaApplication.java
@@ -0,0 +1,13 @@
+package com.social.media.confessionmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfessionMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfessionMediaApplication.class, args);
+	}
+
+}

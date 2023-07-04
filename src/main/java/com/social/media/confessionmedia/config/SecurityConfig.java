7c07d3d08db2f9f554919658ec722f23481ce9c0
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-07-04 12:15:08 +0100

    - Security HTTPBASIC enable username / password - Password Encoder

--- src/main/java/com/social/media/confessionmedia/config/SecurityConfig.java
+++ src/main/java/com/social/media/confessionmedia/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 // This is Spring boot 3, Spring security 6.1
@@ -14,12 +16,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests( authz -> authz
-                        .requestMatchers("/api/auth/**")
+        http.authorizeHttpRequests( (authz) ->
+                authz.requestMatchers("/api/auth/**")
                         .permitAll()
-                        .anyRequest()
-                        .authenticated());
+                        .anyRequest().authenticated()
+        );
 
+        // 401 possible to be authenticated
         http.httpBasic(Customizer.withDefaults());
 
         http.csrf().disable();
@@ -27,4 +30,9 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder factoryPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
+
+}
--- src/main/java/com/social/media/confessionmedia/service/AuthService.java
+++ src/main/java/com/social/media/confessionmedia/service/AuthService.java
@@ -2,27 +2,51 @@ package com.social.media.confessionmedia.service;
 
 import com.social.media.confessionmedia.dto.RegisterForm;
 import com.social.media.confessionmedia.model.User;
+import com.social.media.confessionmedia.model.VerificationToken;
 import com.social.media.confessionmedia.repository.UserRepo;
+import com.social.media.confessionmedia.repository.VerificationTokenRepo;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
+import java.util.UUID;
 
+@AllArgsConstructor
 @Service
 public class AuthService {
 
-    @Autowired
     private UserRepo userRepo;
+    private PasswordEncoder passwordEncoder;
+    private VerificationTokenRepo verificationTokenRepo;
 
+    @Transactional
     public void signUp(RegisterForm registerForm){
         User user = new User();
-
         user.setEmail(registerForm.getEmail());
         user.setUserName(registerForm.getUserName());
-        user.setEnabled(true);
+        user.setEnabled(false);
         user.setCreated(Instant.now());
-        user.setPassword(registerForm.getPassword());
 
+        user.setPassword(passwordEncoder.encode(registerForm.getPassword()));
         userRepo.save(user);
+
+        generateVerification(user);
     }
+
+    private void generateVerification(User user) {
+        String tokenValue =  UUID.randomUUID().toString();
+
+        VerificationToken vt = new VerificationToken();
+        vt.setTokenValue(tokenValue);
+        vt.setUser(user);
+        vt.setCreated(Instant.now());
+        verificationTokenRepo.save(vt);
+    }
+
+
 }

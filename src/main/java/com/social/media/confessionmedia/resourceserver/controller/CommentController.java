6ebc4e15c9cddcb12c9b79f7d6c720845474cb42
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-07-25 16:16:40 +0100

    Implement comment function.

---
+++ src/main/java/com/social/media/confessionmedia/resourceserver/controller/CommentController.java
@@ -0,0 +1,41 @@
+package com.social.media.confessionmedia.resourceserver.controller;
+
+
+import com.social.media.confessionmedia.resourceserver.dto.CommentsDto;
+import com.social.media.confessionmedia.resourceserver.service.CommentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/api/v1/comments")
+@AllArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<Void> createComment(@RequestBody CommentsDto commentsDto) {
+        commentService.save(commentsDto);
+        return new ResponseEntity<>(CREATED);
+    }
+
+    // api/v1/comments?postId=1
+    @GetMapping(params = "postId")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForPost(@RequestParam Long postId) {
+        return ResponseEntity.status(OK)
+                .body(commentService.getAllCommentsForPost(postId));
+    }
+
+    // api/v1/comments?userName=user1
+    @GetMapping(params = "userName")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForUser(@RequestParam String userName){
+        return ResponseEntity.status(OK)
+                .body(commentService.getAllCommentsForUser(userName));
+    }
+
+}
